90854ff386d955ef9adaa7373e8b3aa3313aab26
liushuang <liu1s0404@outlook.com>
2016-06-01 18:41:56 +0800

    修改mysql安装包的地址

--- roles/base_service_manual/files/install_mysql.sh
+++ roles/base_service_manual/files/install_mysql.sh
@@ -9,16 +9,16 @@ if [ -f /usr/local/mysql/bin/mysql ];then
 fi
 
 cd $1
-if [ ! -f "mysql-5.5.45.tar.gz" ];then
-    wget http://mirrors.sohu.com/mysql/MySQL-5.5/mysql-5.5.45.tar.gz
+if [ ! -f "mysql-5.5.49.tar.gz" ];then
+    wget http://mirrors.sohu.com/mysql/MySQL-5.5/mysql-5.5.49.tar.gz
 fi
 
 #tar
-if [ ! -d "mysql-5.5.45" ];then
-    tar zvxf mysql-5.5.45.tar.gz
+if [ ! -d "mysql-5.5.49" ];then
+    tar zvxf mysql-5.5.49.tar.gz
 fi
 
-cd mysql-5.5.45
+cd mysql-5.5.49
 
 #configure mysql
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local/mysql > cmake.log;
